3a4441da7ca1606f845d1940d1a9e64636b50e3b
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-05-10 10:25:53 +0100

    Added Swinub and Piloswine to starter selector

--- engine/menus/options.asm
+++ engine/menus/options.asm
@@ -596,6 +596,8 @@ StarterOptionStringsPointerTable:
 	dw StarterDragoniteText
 	dw StarterMewtwoText
 	dw StarterMewText
+	dw StarterSwinubText
+	dw StarterPiloswineText
 	
 StarterBulbasaurText: 	db "BULBASAUR @", 0
 StarterIvysaurText: 	db "IVYSAUR   @", 0
@@ -748,6 +750,8 @@ StarterDragonairText: 	db "DRAGONAIR @", 0
 StarterDragoniteText: 	db "DRAGONITE @", 0
 StarterMewtwoText: 		db "MEWTWO    @", 0
 StarterMewText: 		db "MEW       @", 0
+StarterSwinubText: 		db "SWINUB    @", 0
+StarterPiloswineText:	db "PILOSWINE @", 0
 
 OptionsMenu_StarterChoice:
 	ld a, [wStarterPokemon]
@@ -763,7 +767,7 @@ OptionsMenu_StarterChoice:
 .pressedRight
 	ld a, c
 	inc a
-	cp 151
+	cp PILOSWINE
 	jr c, .store
 	ld a, 0
 	jr .store
@@ -772,7 +776,7 @@ OptionsMenu_StarterChoice:
 	ld a, c
 	cp 0
 	jr nz, .dec
-	ld c, 151 - 1
+	ld c, PILOSWINE - 1
 	jr .store_from_c
 .dec
 	dec c
