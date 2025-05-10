e6b22b5fba83e2b0d621e1162a4bf93eef409312
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-05-10 10:20:24 +0100

    Added Delibird to Starter Selector

--- engine/menus/options.asm
+++ engine/menus/options.asm
@@ -596,6 +596,7 @@ StarterOptionStringsPointerTable:
 	dw StarterDragoniteText
 	dw StarterMewtwoText
 	dw StarterMewText
+	dw StarterDelibirdText
 	
 StarterBulbasaurText: 	db "BULBASAUR @", 0
 StarterIvysaurText: 	db "IVYSAUR   @", 0
@@ -748,6 +749,7 @@ StarterDragonairText: 	db "DRAGONAIR @", 0
 StarterDragoniteText: 	db "DRAGONITE @", 0
 StarterMewtwoText: 		db "MEWTWO    @", 0
 StarterMewText: 		db "MEW       @", 0
+StarterDelibirdText:	db "DELIBIRD  @", 0
 
 OptionsMenu_StarterChoice:
 	ld a, [wStarterPokemon]
@@ -763,7 +765,7 @@ OptionsMenu_StarterChoice:
 .pressedRight
 	ld a, c
 	inc a
-	cp 151
+	cp DELIBIRD
 	jr c, .store
 	ld a, 0
 	jr .store
@@ -772,7 +774,7 @@ OptionsMenu_StarterChoice:
 	ld a, c
 	cp 0
 	jr nz, .dec
-	ld c, 151 - 1
+	ld c, DELIBIRD - 1
 	jr .store_from_c
 .dec
 	dec c
